8f6a6c3e576284f67335340f5473a33ef94139ea
rinanm <69369179+rinanm@users.noreply.github.com>
2020-08-07 17:25:38 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public class static void main(String[] args) {
+        //Prints "Hello" in the terminal window.
+        System.out.println("Hello");
+        }
+        }
